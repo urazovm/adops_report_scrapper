86847e4a871533f1a2f84c3beca8693f8e2e30f0
Stayman Hou <stayman.hou@gmail.com>
2017-05-11 12:51:43 -0400

    0.2.12

--- lib/adops_report_scrapper/springserve_client.rb
+++ lib/adops_report_scrapper/springserve_client.rb
@@ -1,53 +1,33 @@
 require 'date'
 require_relative 'base_client'
+require 'rest-client'
 
 class AdopsReportScrapper::SpringserveClient < AdopsReportScrapper::BaseClient
   private
 
-  def login
-    @client.visit 'http://video.springserve.com/'
-    @client.fill_in 'Email', :with => @login
-    @client.fill_in 'Password', :with => @secret
-    @client.click_button 'Log in'
-    begin
-      @client.find :xpath, '//*[contains(text(),"Reporting")]'
-    rescue Exception => e
-      raise e, 'Springserve login error'
-    end
+  def init_client
   end
+
+  def before_quit_with_error
+  end
+
+  private
 
   def scrap
-    request_report
-    extract_data_from_report
-  end
+    date_str = @date.strftime('%Y-%m-%d')
 
-  def request_report
-    @client.find(:xpath, '//*[contains(text(),"Reporting")]').click
-    @client.find(:xpath, '//*[contains(text(),"Create Reports")]').click
+    response = RestClient.post "https://video.springserve.com/api/v0/auth", email: @login, password: @secret
+    data = JSON.parse response
+    token = data['token']
 
-    # select date
-    @client.find(:css, '#date_range_chosen').click
-    @client.find(:xpath, '//*[text()="Yesterday"]').click
+    headers = { content_type: :json, accept: :json, authorization: token }
 
-    @client.find(:css, '#dimensions_chosen').click
-    @client.find(:xpath, '//*[text()="Country"]').click
-
-    @client.find(:xpath, '//*[@value="Run Report"]').click
-
-    30.times do |_i| # wait 5 min
-      begin
-        @client.find(:css, '#spinner_image')
-      rescue Exception => e
-        break
-      end
-      sleep 10
+    response = RestClient.post "https://video.springserve.com/api/v0/report", { timezone: 'America/New_York', date_range: 'Yesterday', interval: 'cumulative', dimensions: ['country', 'supply_tag_id']}.to_json, headers
+    data = JSON.parse response
+    header = data[0].keys
+    @data = [header]
+    @data += data.map do |datum|
+      header.map { |key| datum[key] }
     end
-    sleep 10
-  end
-
-  def extract_data_from_report
-    rows = @client.find_all :xpath, '//table[1]/*/tr'
-    @data = rows.map { |tr| tr.find_css('td,th').map { |td| td.visible_text } }.reject { |row| row[0] == 'Total' }
-    @data = [] if @data.flatten.include? 'No data available in table'
   end
 end
